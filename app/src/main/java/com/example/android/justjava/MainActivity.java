a8ab4183c10c266b19d8cb5e71bbd71d6dbb529e
Frank Z <zhengfrancis10@outlook.com>
2017-11-18 15:17:04 +0800

    final vision

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,10 +5,18 @@
  * package com.example.android.justjava;
  */
 package com.example.android.justjava;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
 
 /**
@@ -16,7 +24,7 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,38 +36,94 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity*5);
+        EditText text = (EditText) findViewById(R.id.name);
+        String name = text.getText().toString();
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean addWhippedCream = whippedCreamCheckBox.isChecked();
+        CheckBox chocolateChickBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean addChocolate = chocolateChickBox.isChecked();
+        int price = calculatePrice(addWhippedCream, addChocolate);
+        composeEmail(createOrderSummary(name, price, addWhippedCream, addChocolate),name);
+        //displayMessage(priceMessage);
+    }
+
+    private String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate) {
+        String priceMessage;
+        priceMessage = getString(R.string.order_summary_name, name);
+        priceMessage += "\n" + getString(R.string.order_summary_whipped_cream, addWhippedCream);
+        priceMessage += "\n" + getString(R.string.order_summary_chocolate, addChocolate);
+        priceMessage += "\n" + getString(R.string.order_summary_quantity, quantity);
+        priceMessage += "\n" + getString(R.string.order_summary_price, NumberFormat.getCurrencyInstance().format(price));
+        priceMessage += "\n" + getString(R.string.thank_you);
+        return (priceMessage);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
     /**
      * This method is called when the increase button is clicked.
      */
     public void increment(View view) {
-        quantity = quantity + 1;
-        display(quantity);
+        if (100 > quantity) {
+            displayQuantity(++quantity);
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "You cannot have more than 100 coffee";
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+            return;
+        }
+
     }
 
     /**
      * This method is called when the decrease button is clicked.
      */
     public void decrement(View view) {
-        quantity = quantity - 1;
-        display(quantity);
+        if (1 < quantity) {
+            displayQuantity(--quantity);
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "You cannot have less than 1 coffee";
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+            return;
+        }
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @return the total price.
+     */
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int singlePrice = 5;
+        if (addWhippedCream) {
+            ++singlePrice;
+        }
+        if (addChocolate) {
+            singlePrice += 2;
+        }
+        return (quantity * singlePrice);
+    }
+
+    /**
+     * Send the content of order summary to the costumer with an email.
+     */
+    public void composeEmail(String text,String name) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.order_summary_email_subject, name));
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
